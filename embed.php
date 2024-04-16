28d5ecfa9678c455add1913638393130b421fd81
Harry <150482800+hpro97@users.noreply.github.com>
2024-04-16 21:50:29 +0100

    playground file

---
+++ embed.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <?php 
+   $title = "Hello World, i'm learning PHP"
+   ?>
+
+   <?php
+   //this function below is an echo, this here is a comment
+
+   /*this is a multiline comment in PHP
+   line 2
+   */
+   echo "This is an echo"
+   ?>
+   <h1><?php echo $title;?></h1>
+</body>
+</html>
